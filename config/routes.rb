fe19d71d75311d4ccaa0e2d5a07e0143bd622d46
ei-lene <eileneheng@gmail.com>
2013-04-08 21:12:58 -0400

    Add scaffold Location

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MapSearch::Application.routes.draw do
+  resources :locations
+  
+  root :to => 'locations#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,10 +49,6 @@ MapSearch::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
