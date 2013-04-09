7e86083833a40ed8fb888419f00a7d860d3f0284
ei-lene <eileneheng@gmail.com>
2013-04-08 21:06:30 -0400

    Generate new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapSearch::Application.config.secret_token = 'fe91223d1fd2068983f5f4b6bfd0e3990e5b7559a1555ded317c30ef1e8fbe5e2684f6853f6ae133ad1f65c2de4b90ca6c0b2bf4883cf73a7375683e866c897f'
